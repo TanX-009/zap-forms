a4e9a24c0a71e0b3ca81efa8d879828ae5dc1158
TanX-009 <tanmaymuley009@gmail.com>
2025-04-02 13:47:54 +0530

    fix(FE: cookies): add await for deletion

--- frontend/src/app/actions/cookies.ts
+++ frontend/src/app/actions/cookies.ts
@@ -5,6 +5,6 @@ import { cookies } from "next/headers";
 export async function deleteLogin() {
   const cookieStore = await cookies();
   await cookieStore.delete("login");
-  cookieStore.delete("access_token");
-  cookieStore.delete("refresh_token");
+  await cookieStore.delete("access_token");
+  await cookieStore.delete("refresh_token");
 }
